7122ad38f46f27299edb842b2718ff1105368440
spoyodevelop <kjy9505@gmail.com>
2024-12-12 16:24:56 +0900

    feat: promo 재고만 있는 제품을 nonPromo도 추가

--- src/App.js
+++ src/App.js
@@ -5,9 +5,25 @@ import Product from './Model/Product.js';
 class App {
   async run() {
     const parsedProduct = new Parser('public/products.md').parsedArray;
+
+    const allProductList = [
+      ...new Set(parsedProduct.map((product) => product[0])),
+    ];
+    const noPromoProductList = parsedProduct
+      .filter((product) => product[3] === 'null')
+      .map((product) => product[0]);
+    const differenceList = allProductList.filter(
+      (x) => !noPromoProductList.includes(x),
+    );
+
+    // parsedProduct.map(product => )s
+    // parsedProduct.filter();
     const products = [];
     parsedProduct.forEach(([name, price, quantity, promotion]) => {
       products.push(new Product(name, price, quantity, promotion));
+      if (differenceList.includes(name)) {
+        products.push(new Product(name, price, 0, 'null'));
+      }
     });
 
     products.forEach((product) => console.log(product.toString()));
--- src/Model/Product.js
+++ src/Model/Product.js
@@ -11,8 +11,8 @@ class Product {
 
   constructor(name, price, quantity, promotion) {
     this.#name = name;
-    this.#price = price;
-    this.#quantity = quantity;
+    this.#price = +price;
+    this.#quantity = +quantity;
     this.#promotion = new Promotion(promotion);
   }
 
