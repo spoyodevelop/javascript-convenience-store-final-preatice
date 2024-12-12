2e07f692103743f5016b25ff8194eeb86caf4d5d
spoyodevelop <kjy9505@gmail.com>
2024-12-12 20:28:47 +0900

    feat(processTotal): 영수증을 total을 process하는 processTotal을 작성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import { Console } from '@woowacourse/mission-utils';
-
 import OutputView from './View/OutputView.js';
 import InputView from './View/InputView.js';
 import ProductMaker from './ProductMaker.js';
@@ -9,15 +7,20 @@ import BillProcessor from './BillProcessor.js';
 class App {
   async run() {
     const { products } = new ProductMaker();
-    OutputView.displayProducts(products);
+
     const shoppingCart = await InputView.getShoppingCart(products);
+    const membership = await InputView.askUserAgree(
+      '맴버쉽 할인을 받으시겠습니까? (Y/N)',
+    );
     const bills = [];
     for (const shoppingItem of shoppingCart) {
       bills.push(await ProductSeller(shoppingItem, products));
     }
+
     const billProcessor = new BillProcessor();
     const eachOne = billProcessor.processEachOne(bills);
     const freebieBill = billProcessor.processFreebie(bills);
+    const totalBill = billProcessor.processTotal(bills, membership);
   }
 }
 
--- src/BillProcessor.js
+++ src/BillProcessor.js
@@ -24,5 +24,43 @@ class BillProcessor {
         freebie,
       }));
   }
+
+  processTotal(bills, membership) {
+    let totalBeforeDiscount = 0;
+    let total = 0;
+    let promoDiscount = 0;
+    let membershipDiscount = 0;
+    let totalQuantity = 0;
+    bills.forEach(
+      ({
+        leftovers,
+        promoSellQuantity,
+        nonPromoSellingQuantity,
+        price,
+        freebie,
+      }) => {
+        totalQuantity +=
+          leftovers + promoSellQuantity + nonPromoSellingQuantity;
+        totalBeforeDiscount +=
+          price * (leftovers + promoSellQuantity + nonPromoSellingQuantity);
+        promoDiscount += price * freebie;
+        membershipDiscount +=
+          price * (leftovers + nonPromoSellingQuantity) * 0.3;
+      },
+    );
+    if (!membership) {
+      membershipDiscount = 0;
+    }
+    membershipDiscount = Math.min(8000, membershipDiscount);
+    total = totalBeforeDiscount - promoDiscount - membershipDiscount;
+
+    return {
+      totalBeforeDiscount,
+      totalQuantity,
+      promoDiscount,
+      membershipDiscount,
+      total,
+    };
+  }
 }
 export default BillProcessor;
