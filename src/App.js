8daff61796a2e4d32e50a3d892e14c05de029364
spoyodevelop <kjy9505@gmail.com>
2024-12-12 17:26:44 +0900

    refactor: product를 만드는 ProductMaker를 작성

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import { Console } from '@woowacourse/mission-utils';
-import Parser from './Parser.js';
-import Product from './Model/Product.js';
+
 import OutputView from './View/OutputView.js';
 import InputView from './View/InputView.js';
+import ProductMaker from './ProductMaker.js';
 
 class App {
   async run() {
-    const parsedProduct = new Parser('public/products.md').parsedArray;
-
-    const allProductList = [
-      ...new Set(parsedProduct.map((product) => product[0])),
-    ];
-    const noPromoProductList = parsedProduct
-      .filter((product) => product[3] === 'null')
-      .map((product) => product[0]);
-    const differenceList = allProductList.filter(
-      (x) => !noPromoProductList.includes(x),
-    );
-
-    const products = [];
-    parsedProduct.forEach(([name, price, quantity, promotion]) => {
-      products.push(new Product(name, price, quantity, promotion));
-      if (differenceList.includes(name)) {
-        products.push(new Product(name, price, 0, 'null'));
-      }
-    });
-
-    // OutputView.displayProducts(products);
-    const input = await InputView.getShoppingCart(products);
-    console.log(input);
+    const { products } = new ProductMaker();
+    OutputView.displayProducts(products);
+    // const input = await InputView.getShoppingCart(products);
   }
 }
 
--- src/ProductMaker.js
+++ src/ProductMaker.js
@@ -1,3 +1,34 @@
+import Parser from './Parser.js';
+
+import Product from './Model/Product.js';
+
 class ProductMaker {
-  constructor() {}
+  #products;
+
+  constructor() {
+    const parsedProduct = new Parser('public/products.md').parsedArray;
+    const allProductList = [
+      ...new Set(parsedProduct.map((product) => product[0])),
+    ];
+    const noPromoProductList = parsedProduct
+      .filter((product) => product[3] === 'null')
+      .map((product) => product[0]);
+    const differenceList = allProductList.filter(
+      (x) => !noPromoProductList.includes(x),
+    );
+    const products = [];
+    parsedProduct.forEach(([name, price, quantity, promotion]) => {
+      products.push(new Product(name, price, quantity, promotion));
+      if (differenceList.includes(name)) {
+        products.push(new Product(name, price, 0, 'null'));
+      }
+    });
+
+    this.#products = products;
+  }
+
+  get products() {
+    return this.#products;
+  }
 }
+export default ProductMaker;
